e130a236b0ec404cc3ca430579d7b270a7a8c17b
1E10 Laptop <sample@bths.edu>
2022-09-22 10:47:50 -0400

    update

--- src/TipCalculatorProject.java
+++ src/TipCalculatorProject.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class TipCalculatorProject {
+import java.util.Scanner;
+import java.text.DecimalFormat;
+
+public class TipCalculatorProject {
+    public static void main(String[] args) {
+        DecimalFormat formatter = new DecimalFormat("#.##");
+        System.out.println("Welcome to the tip calculator!");
+    }
+
 }
